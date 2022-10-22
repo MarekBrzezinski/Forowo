2dbda2ce8cef92eb3cc071bc81557c612b710e97
Marek Brzezinski <brzezi14@gmail.com>
2022-10-22 21:11:17 +0200

    changed function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-console.log("To jest projekt strony internetowej dla FOROWO");
+{
+  const welcome = () => {
+    console.log("To jest projekt strony internetowej dla FOROWO"); };
+  
+  const onChangeHiddenPriceOffer = () => {
+    const main  = document.querySelector(".js-main__hiddenPriceOffer");
+    const button = document.querySelector(".js-main__button");
+    main.classList.toggle("main__hiddenPriceOffer");
+    button.innerText = main.classList.contains("main__hiddenPriceOffer")
+      ? "Pokaż ofertę cenową"
+      : "Ukryj ofertę cenową"; 
+  };
+  
+  const init = () => {
+    
+    const button = document.querySelector(".js-main__button");
+    
+    button.addEventListener("click", onChangeHiddenPriceOffer);
+  
+    welcome ()
+  };
 
-let button = document.querySelector(".js-main__button");
-let main  = document.querySelector(".js-main__hiddenPriceOffer");
-
-button.addEventListener("click", () => {
-  main.classList.toggle("main__hiddenPriceOffer");
-  button.innerText = main.classList.contains("main__hiddenPriceOffer")
-    ? "Pokaż ofertę cenową"
-    : "Ukryj ofertę cenową"; 
-});
+  init ();
+}
