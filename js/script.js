3c9b9684bbfd57c067d892c4ab961cdef310762e
Marek Brzezinski <brzezi14@gmail.com>
2022-10-24 23:01:17 +0200

    changed name functions and name parametrs

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 {
   const welcome = () => {
-    console.log("To jest projekt strony internetowej dla FOROWO"); };
+    console.log("To jest projekt strony internetowej dla FOROWO");
+  };
   
-  const onChangeHiddenPriceOffer = () => {
-    const main  = document.querySelector(".js-main__hiddenPriceOffer");
-    const button = document.querySelector(".js-main__button");
-    main.classList.toggle("main__hiddenPriceOffer");
-    button.innerText = main.classList.contains("main__hiddenPriceOffer")
+  const togglePriceOffer = () => {
+    const mainElement  = document.querySelector(".js-main__hiddenPriceOffer");
+    const buttonElement = document.querySelector(".js-main__button");
+    mainElement.classList.toggle("main__hiddenPriceOffer");
+    buttonElement.innerText = mainElement.classList.contains("main__hiddenPriceOffer")
       ? "Pokaż ofertę cenową"
       : "Ukryj ofertę cenową"; 
   };
   
   const init = () => {
     
-    const button = document.querySelector(".js-main__button");
+    const buttonElement = document.querySelector(".js-main__button");
     
-    button.addEventListener("click", onChangeHiddenPriceOffer);
+    buttonElement.addEventListener("click",togglePriceOffer);
   
     welcome ()
   };
