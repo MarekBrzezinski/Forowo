97f5eed5c64bded9262094b49d065d7360828070
Marek Brzezinski <brzezi14@gmail.com>
2022-11-05 16:35:29 +0100

    added new function and renamed hidden class

--- js/script.js
+++ js/script.js
@@ -3,22 +3,26 @@
     console.log("To jest projekt strony internetowej dla FOROWO");
   };
   
-  const togglePriceOffer = () => {
-    const mainElement  = document.querySelector(".js-main__hiddenPriceOffer");
+  const changeNameButton = (mainElement) => {
     const buttonElement = document.querySelector(".js-main__button");
-    mainElement.classList.toggle("main__hiddenPriceOffer");
-    buttonElement.innerText = mainElement.classList.contains("main__hiddenPriceOffer")
-      ? "Pokaż ofertę cenową"
-      : "Ukryj ofertę cenową"; 
+
+    buttonElement.innerText = mainElement.classList.contains("hidden")
+        ? "Pokaż ofertę cenową"
+        : "Ukryj ofertę cenową"; 
+  };
+
+  const togglePriceOffer = () => {
+    const mainElement  = document.querySelector(".js-hidden");
+    mainElement.classList.toggle("hidden");
+
+    changeNameButton(mainElement);
   };
   
   const init = () => {
-    
     const buttonElement = document.querySelector(".js-main__button");
-    
     buttonElement.addEventListener("click",togglePriceOffer);
   
-    welcome ()
+    welcome ();
   };
 
   init ();
